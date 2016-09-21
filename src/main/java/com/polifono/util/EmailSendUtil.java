63805c731eedc5b2cef6727a5ab7537f821dc512
Grimm <Grimm@Grimm-PC>
2016-09-21 15:05:30 -0300

    email

--- src/main/java/com/polifono/util/EmailSendUtil.java
+++ src/main/java/com/polifono/util/EmailSendUtil.java
@@ -1,99 +1,111 @@
 package com.polifono.util;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import org.apache.commons.mail.MultiPartEmail;
 
 public class EmailSendUtil {
 	
-	public static void main(String args[]) {
-		try {
-			String[] args2 = new String[3];
-			args2[0] = "Flávio Moisés de Araújo";
-			args2[1] = "João Tavares";
-			args2[2] = "Turma Nono Ano";
-			EmailSendUtil.sendHtmlMail(4, "flavio10araujo@gmail.com", args2);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+	private static ResourceBundle resourceBundle;
+	
+	static {
+		 resourceBundle = ResourceBundle.getBundle("application", Locale.getDefault());
 	}
 	
 	public static void sendHtmlMail(int messageType, String to, String[] args) throws Exception {
 		String from = "", subject = "", message = "";
 		
 		if (messageType == 1) {
-			from = "new@polifono.com";
-			subject = "Confirme seu cadastro na Polifono.com";
+			from = resourceBundle.getString("email.noreply");
+			subject = "Confirme seu cadastro na " + resourceBundle.getString("email.company");
 			message = "<table cellpadding=\"0\" cellspacing=\"0\" align=\"center\" width=\"550\" summary=\"\">"
-					+ "<thead><tr><td align=\"center\"><font color=\"#ff8533\" face=\"arial\" size=\"+2\"><b>POLIFONO.COM</b></font><hr size=\"3\" color=\"#ff8533\"></td></tr></thead><tbody><tr><td>"
+					+ "<thead><tr><td align=\"center\"><font color=\"#ff8533\" face=\"arial\" size=\"+2\" style=\"text-transform:uppercase\"><b>" + resourceBundle.getString("email.company") + "</b></font><hr size=\"3\" color=\"#ff8533\"></td></tr></thead><tbody><tr><td>"
 					+ "<p><font color=\"#ff8533\" face=\"arial\" size=\"+1\"><b>Olá {0},</b></font></p>"
-					+ "<p><font face=\"arial\" size=\"2\">Você iniciou seu cadastro na plataforma Polifono.com com o e-mail <a href=\"#\">{1}</a>. Para finalizá-lo, precisamos que você valide seu email.</font></p>"
+					+ "<p><font face=\"arial\" size=\"2\">Você iniciou seu cadastro na plataforma " + resourceBundle.getString("email.company") + " com o e-mail <a href=\"#\">{1}</a>. Para finalizá-lo, precisamos que você valide seu email.</font></p>"
 					+ "<p><font face=\"arial\" size=\"2\">Para validar:</font></p>"
-					+ "<p><font face=\"arial\" size=\"2\">- Acesse <a href=\"http://www.polifono.com/emailconfirmation\">www.polifono.com/emailconfirmation</a></font></p>"
+					+ "<p><font face=\"arial\" size=\"2\">- Acesse <a href=\"http://" + resourceBundle.getString("email.url") + "/emailconfirmation\">" + resourceBundle.getString("email.url") + "/emailconfirmation</a></font></p>"
 					+ "<p><font face=\"arial\" size=\"2\">- Informe o e-mail cadastrado;</font></p>"
 					+ "<p><font face=\"arial\" size=\"2\">- Informe o seguinte código de ativação: <b>{2}</b></font></p>"
 					+ "</td></tr><tr><td>"
-					+ "<font face=\"arial\" size=\"-1\"><br>Atenciosamente,<br>Equipe Polifono.com<br /><br /><strong>Polifono.com</strong> - A vida sem música seria um engano.</font>"
+					+ "<font face=\"arial\" size=\"-1\"><br>Atenciosamente,<br>Equipe " + resourceBundle.getString("email.company") + "<br /><br /><strong>" + resourceBundle.getString("email.company") + "</strong> - " + resourceBundle.getString("email.company.slogan") + "</font>"
 					+ "</td></tr><tr><td align=\"center\">"
-					+ "<hr size=\"2\" color=\"#EFEFEF\"><font face=\"arial\" size=\"-1\">DÚVIDAS? Acesse <a href=\"http://www.polifono.com/faq\">www.polifono.com/faq</a></font>"
+					+ "<hr size=\"2\" color=\"#EFEFEF\"><font face=\"arial\" size=\"-1\">DÚVIDAS? Acesse <a href=\"http://" + resourceBundle.getString("email.url") + "/faq\">" + resourceBundle.getString("email.url") + "/faq</a></font>"
 					+ "</td></tr><tr><td align=\"center\"><hr size=\"2\" color=\"#EFEFEF\"><font face=\"arial\" size=\"-1\">Este é um e-mail automático disparado pelo sistema. Favor não respondê-lo, pois esta conta não é monitorada.</font>"
 					+ "</td></tr></tbody></table>";
 
 			message = replaceParamsMessage(message, args);
 		}
 		else if (messageType == 2) {
-			from = "resetpassword@polifono.com";
-			subject = "Sua soliticação de alteração de senha na Polifono.com";
+			from = resourceBundle.getString("email.noreply");
+			subject = "Sua soliticação de alteração de senha na " + resourceBundle.getString("email.company");
 			message = "<table cellpadding=\"0\" cellspacing=\"0\" align=\"center\" width=\"550\" summary=\"\">"
-					+ "<thead><tr><td align=\"center\"><font color=\"#ff8533\" face=\"arial\" size=\"+2\"><b>POLIFONO.COM</b></font><hr size=\"3\" color=\"#ff8533\"></td></tr></thead><tbody><tr><td>"
+					+ "<thead><tr><td align=\"center\"><font color=\"#ff8533\" face=\"arial\" size=\"+2\" style=\"text-transform:uppercase\"><b>" + resourceBundle.getString("email.company") + "</b></font><hr size=\"3\" color=\"#ff8533\"></td></tr></thead><tbody><tr><td>"
 					+ "<p><font color=\"#ff8533\" face=\"arial\" size=\"+1\"><b>Olá {0},</b></font></p>"
-					+ "<p><font face=\"arial\" size=\"2\">Você (ou alguém) solicitou a alteração da senha na plataforma Polifono.com para o e-mail <a href=\"#\">{1}</a>.</font></p>"
-					+ "<p><font face=\"arial\" size=\"2\">Para dar continuidade na alteraração da senha:</font></p>"
-					+ "<p><font face=\"arial\" size=\"2\">- Acesse <a href=\"http://www.polifono.com/passwordreset\">www.polifono.com/passwordreset</a></font></p>"
+					+ "<p><font face=\"arial\" size=\"2\">Você (ou alguém) solicitou a alteração da senha na plataforma " + resourceBundle.getString("email.company") + " para o e-mail <a href=\"#\">{1}</a>.</font></p>"
+					+ "<p><font face=\"arial\" size=\"2\">Para dar continuidade na alteração da senha:</font></p>"
+					+ "<p><font face=\"arial\" size=\"2\">- Acesse <a href=\"http://" + resourceBundle.getString("email.url") + "/passwordreset\">" + resourceBundle.getString("email.url") + "/passwordreset</a></font></p>"
 					+ "<p><font face=\"arial\" size=\"2\">- Informe o e-mail cadastrado;</font></p>"
 					+ "<p><font face=\"arial\" size=\"2\">- Informe o seguinte código de confirmação da alteração da senha: <b>{2}</b></font></p>"
 					+ "<p><font face=\"arial\" size=\"2\">- Informe a nova senha desejada.</font></p>"
 					+ "<p><font face=\"arial\" size=\"2\"><br /><br />Caso a alteração de senha não seja mais necessária, apenas ignore este e-mail.</font></p>"
 					+ "</td></tr><tr><td>"
-					+ "<font face=\"arial\" size=\"-1\"><br />Atenciosamente,<br />Equipe Polifono.com<br /><br /><strong>Polifono.com</strong> - A vida sem música seria um engano.</font>"
+					+ "<font face=\"arial\" size=\"-1\"><br />Atenciosamente,<br />Equipe " + resourceBundle.getString("email.company") + "<br /><br /><strong>" + resourceBundle.getString("email.company") + "</strong> - " + resourceBundle.getString("email.company.slogan") + "</font>"
 					+ "</td></tr><tr><td align=\"center\">"
-					+ "<hr size=\"2\" color=\"#EFEFEF\"><font face=\"arial\" size=\"-1\">DÚVIDAS? Acesse <a href=\"http://www.polifono.com/faq\">www.polifono.com/faq</a></font>"
+					+ "<hr size=\"2\" color=\"#EFEFEF\"><font face=\"arial\" size=\"-1\">DÚVIDAS? Acesse <a href=\"http://" + resourceBundle.getString("email.url") + "/faq\">" + resourceBundle.getString("email.url") + "/faq</a></font>"
 					+ "</td></tr><tr><td align=\"center\"><hr size=\"2\" color=\"#EFEFEF\"><font face=\"arial\" size=\"-1\">Este é um e-mail automático disparado pelo sistema. Favor não respondê-lo, pois esta conta não é monitorada.</font>"
 					+ "</td></tr></tbody></table>";
 			
 			message = replaceParamsMessage(message, args);
 		}
 		else if (messageType == 3) {
-			from = "polifono@polifono.com";
-			subject = "Sua compra de créditos foi confirmada na Polifono.com";
+			from = resourceBundle.getString("email.general");
+			subject = "Sua compra de créditos foi confirmada na " + resourceBundle.getString("email.company");
 			message = "<table cellpadding=\"0\" cellspacing=\"0\" align=\"center\" width=\"550\" summary=\"\">"
-					+ "<thead><tr><td align=\"center\"><font color=\"#ff8533\" face=\"arial\" size=\"+2\"><b>POLIFONO.COM</b></font><hr size=\"3\" color=\"#ff8533\"></td></tr></thead><tbody><tr><td>"
+					+ "<thead><tr><td align=\"center\"><font color=\"#ff8533\" face=\"arial\" size=\"+2\" style=\"text-transform:uppercase\"><b>" + resourceBundle.getString("email.company") + "</b></font><hr size=\"3\" color=\"#ff8533\"></td></tr></thead><tbody><tr><td>"
 					+ "<p><font color=\"#ff8533\" face=\"arial\" size=\"+1\"><b>Olá {0},</b></font></p>"
 					+ "<p><font face=\"arial\" size=\"2\">A sua compra foi confirmada e os créditos adquiridos já estão disponíveis em sua conta.</font></p>"
 					+ "<p><font face=\"arial\" size=\"2\"><br />O total de créditos adquiridos foi de: <b>{1}</b></font></p>"
 					+ "<p><font face=\"arial\" size=\"2\">Obs.: caso esteja logado no sistema, é necessário deslogar e logar novamente para que os novos créditos sejam mostrados.</b></font></p>"
 					+ "<p><font face=\"arial\" size=\"2\"><br /><br />Não perca tempo e comece a estudar agora mesmo.</font></p>"
 					+ "</td></tr><tr><td>"
-					+ "<font face=\"arial\" size=\"-1\"><br />Atenciosamente,<br />Equipe Polifono.com<br /><br /><strong>Polifono.com</strong> - A vida sem música seria um engano.</font>"
+					+ "<font face=\"arial\" size=\"-1\"><br />Atenciosamente,<br />Equipe " + resourceBundle.getString("email.company") + "<br /><br /><strong>" + resourceBundle.getString("email.company") + "</strong> - " + resourceBundle.getString("email.company.slogan") + "</font>"
 					+ "</td></tr><tr><td align=\"center\">"
-					+ "<hr size=\"2\" color=\"#EFEFEF\"><font face=\"arial\" size=\"-1\">DÚVIDAS? Acesse <a href=\"http://www.polifono.com/faq\">www.polifono.com/faq</a></font>"
+					+ "<hr size=\"2\" color=\"#EFEFEF\"><font face=\"arial\" size=\"-1\">DÚVIDAS? Acesse <a href=\"http://" + resourceBundle.getString("email.url") + "/faq\">" + resourceBundle.getString("email.url") + "/faq</a></font>"
 					+ "</td></tr><tr><td align=\"center\"><hr size=\"2\" color=\"#EFEFEF\">"
 					+ "</td></tr></tbody></table>";
 			
 			message = replaceParamsMessage(message, args);
 		}
 		else if (messageType == 4) {
-			from = "polifono@polifono.com";
-			subject = "Você foi convidado para uma sala de aula na Polifono.com";
+			from = resourceBundle.getString("email.general");
+			subject = "Você foi convidado para uma sala de aula na " + resourceBundle.getString("email.company");
 			message = "<table cellpadding=\"0\" cellspacing=\"0\" align=\"center\" width=\"550\" summary=\"\">"
-					+ "<thead><tr><td align=\"center\"><font color=\"#ff8533\" face=\"arial\" size=\"+2\"><b>POLIFONO.COM</b></font><hr size=\"3\" color=\"#ff8533\"></td></tr></thead><tbody><tr><td>"
+					+ "<thead><tr><td align=\"center\"><font color=\"#ff8533\" face=\"arial\" size=\"+2\" style=\"text-transform:uppercase\"><b>" + resourceBundle.getString("email.company") + "</b></font><hr size=\"3\" color=\"#ff8533\"></td></tr></thead><tbody><tr><td>"
 					+ "<p><font color=\"#ff8533\" face=\"arial\" size=\"+1\"><b>Olá {0},</b></font></p>"
 					+ "<p><font face=\"arial\" size=\"2\">O professor {1} te adicionou na sala de aula {2}.</font></p>"
 					+ "<p><font face=\"arial\" size=\"2\"><br />Caso você queira participar dessa sala de aula, acesse o seguinte link para confirmar sua participação:</font></p>"
-					+ "<p><font face=\"arial\" size=\"2\"><br /><br /><a href=\"http://www.polifono.com/classinvitation\">www.polifono.com/classinvitation</a></font></p>"
+					+ "<p><font face=\"arial\" size=\"2\"><br /><br /><a href=\"http://" + resourceBundle.getString("email.url") + "/classinvitation\">" + resourceBundle.getString("email.url") + "/classinvitation</a></font></p>"
 					+ "</td></tr><tr><td>"
-					+ "<font face=\"arial\" size=\"-1\"><br />Atenciosamente,<br />Equipe Polifono.com<br /><br /><strong>Polifono.com</strong> - A vida sem música seria um engano.</font>"
+					+ "<font face=\"arial\" size=\"-1\"><br />Atenciosamente,<br />Equipe " + resourceBundle.getString("email.company") + "<br /><br /><strong>" + resourceBundle.getString("email.company") + "</strong> - " + resourceBundle.getString("email.company.slogan") + "</font>"
 					+ "</td></tr><tr><td align=\"center\">"
-					+ "<hr size=\"2\" color=\"#EFEFEF\"><font face=\"arial\" size=\"-1\">DÚVIDAS? Acesse <a href=\"http://www.polifono.com/faq\">www.polifono.com/faq</a></font>"
+					+ "<hr size=\"2\" color=\"#EFEFEF\"><font face=\"arial\" size=\"-1\">DÚVIDAS? Acesse <a href=\"http://"+ resourceBundle.getString("email.url") + "/faq\">" + resourceBundle.getString("email.url") + "/faq</a></font>"
+					+ "</td></tr><tr><td align=\"center\"><hr size=\"2\" color=\"#EFEFEF\">"
+					+ "</td></tr></tbody></table>";
+			
+			message = replaceParamsMessage(message, args);
+		}
+		else if (messageType == 5) {
+			from = resourceBundle.getString("email.general");
+			subject = "Novo contato: " + args[0];
+			message = "<table cellpadding=\"0\" cellspacing=\"0\" align=\"center\" width=\"550\" summary=\"\">"
+					+ "<thead><tr><td align=\"center\"><font color=\"#ff8533\" face=\"arial\" size=\"+2\" style=\"text-transform:uppercase\"><b>" + resourceBundle.getString("email.company") + "</b></font><hr size=\"3\" color=\"#ff8533\"></td></tr></thead><tbody><tr><td>"
+					+ "<p><font color=\"#ff8533\" face=\"arial\" size=\"+1\"><b>A seguinte mensagem foi enviada por {0}:</b></font></p>"
+					+ "<p><font face=\"arial\" size=\"2\">Mensagem: {1} </font></p>"
+					+ "</td></tr><tr><td>"
+					+ "<font face=\"arial\" size=\"-1\"><br />Atenciosamente,<br />Equipe " + resourceBundle.getString("email.company") + "<br /><br /><strong>" + resourceBundle.getString("email.company") + "</strong> - " + resourceBundle.getString("email.company.slogan") + "</font>"
+					+ "</td></tr><tr><td align=\"center\">"
+					+ "<hr size=\"2\" color=\"#EFEFEF\"><font face=\"arial\" size=\"-1\">DÚVIDAS? Acesse <a href=\"http://" + resourceBundle.getString("email.url") + "/faq\">" + resourceBundle.getString("email.url") + "/faq</a></font>"
 					+ "</td></tr><tr><td align=\"center\"><hr size=\"2\" color=\"#EFEFEF\">"
 					+ "</td></tr></tbody></table>";
 			
@@ -122,7 +134,6 @@ public class EmailSendUtil {
 			try	{
 				MultiPartEmail hm = new MultiPartEmail();
 				
-				
 				/*
 				Gmail configuration.
 				hm.setHostName("smtp.gmail.com");
@@ -131,11 +142,19 @@ public class EmailSendUtil {
 				hm.setAuthentication("email@gmail.com", "senha");
 				hm.setTLS(true);
 				*/
-				
+
 				// Integrator configuration.
-				hm.setHostName("localhost"); 
-				hm.setSmtpPort(Integer.parseInt("25")); 
-				hm.setAuthentication("email", "senha"); 
+				// TODO - verificar o motivo de funciona localhost/25 pra um e não funcionar pro outro.
+				if (resourceBundle.getString("email.url").equals("www.polifono.com")) {
+					hm.setHostName("localhost");
+					hm.setSmtpPort(Integer.parseInt("25"));
+				}
+				else if (resourceBundle.getString("email.url").equals("www.hbmelhoriadenegocios.net")) {
+					hm.setHostName("mail.hbmelhoriadenegocios.net");
+					hm.setSmtpPort(Integer.parseInt("587"));
+				}
+
+				hm.setAuthentication(resourceBundle.getString("email.general"), resourceBundle.getString("email.general.password"));
 				
 				hm.setCharset("UTF-8");
 				hm.setSubject(subject);
