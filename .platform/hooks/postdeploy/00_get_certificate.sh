b0ff92a1d4abe1cc9903dbc6ea3a04e39c77a804
Flavio Moises de Araujo <flavio.dearaujo@ticketmaster.com>
2023-12-12 15:03:00 -0500

    Trying to make certbot works in Amazon Linux 2023.

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d polifono.com -d www.polifono.com --apache --agree-tos --email flavio10araujo@gmail.com
+sudo certbot -v -n -d polifono.com -d www.polifono.com --apache --agree-tos --email flavio10araujo@gmail.com
