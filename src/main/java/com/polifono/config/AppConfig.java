0fce2d4a0fb043c9b94af8cc16ff8911897e4493
Flavio Moises de Araujo <flavio.dearaujo@ticketmaster.com>
2024-09-08 22:35:46 -0400

    revert print login and password for email

--- src/main/java/com/polifono/config/AppConfig.java
+++ src/main/java/com/polifono/config/AppConfig.java
@@ -1,8 +1,5 @@
 package com.polifono.config;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,34 +14,5 @@ public class AppConfig implements WebMvcConfigurer {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("messages/messages");
         return messageSource;
-    }
-
-    @Value("${email.authentication.login}")
-    private String emailLogin;
-
-    @Value("${email.authentication.password}")
-    private String emailPassword;
-
-    /*@Value("$(credential.email}")
-    private String credentialEmail;
-
-    @Value("${credential.production.token}")
-    private String credentialProductionToken;
-
-    @Value("${credential.production.appId}")
-    private String credentialProductionAppId;
-
-    @Value("${credential.production.appKey}")
-    private String credentialProductionAppKey;*/
-
-    @PostConstruct
-    public void init() {
-        System.out.println("Email login: " + emailLogin);
-        //System.out.println("Email password: " + (emailPassword != null ? "*****" : "Not Set"));
-        System.out.println("Email password: " + emailPassword);
-        //System.out.println("Credential email: " + credentialEmail);
-        //System.out.println("Credential production token: " + credentialProductionToken);
-        //System.out.println("Credential production appId: " + credentialProductionAppId);
-        //System.out.println("Credential production appKey: " + credentialProductionAppKey);
     }
 }
