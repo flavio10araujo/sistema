4062c4019d0ec45425e826f641279dc483641e23
Flavio Moises de Araujo <flavio.dearaujo@ticketmaster.com>
2024-09-24 22:48:47 -0400

    dtos

--- src/main/java/com/polifono/dto/RankingDTO.java
+++ src/main/java/com/polifono/dto/RankingDTO.java
@@ -2,21 +2,4 @@ package com.polifono.dto;
 
 import com.polifono.domain.Player;
 
-public class RankingDTO {
-
-	private Player player;
-	private int score;
-	
-	public Player getPlayer() {
-		return player;
-	}
-	public void setPlayer(Player player) {
-		this.player = player;
-	}
-	public int getScore() {
-		return score;
-	}
-	public void setScore(int score) {
-		this.score = score;
-	}
-}
+public record RankingDTO(Player player, int score) {}
