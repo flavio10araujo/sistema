8e4139449dc6c74fce9e022d5b8fc859232e8bf2
Flavio Moises de Araujo <flavio.dearaujo@ticketmaster.com>
2024-10-09 21:59:02 -0400

    model

--- src/main/java/com/polifono/domain/enums/Rank.java
+++ src/main/java/com/polifono/domain/enums/Rank.java
@@ -1,30 +1,26 @@
 package com.polifono.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Rank {
-	WHITE(1, "Faixa Branca"), 
-	YELLOW(2, "Faixa Amarela"), 
-	ORANGE(3, "Faixa Laranjada"), 
-	RED(4, "Faixa Vermelha"), 
-	PURPLE(5, "Faixa Roxa"), 
-	BROWN(6, "Faixa Marrom"), 
-	BLACK(7, "Faixa Preta"), 
-	COPPER(8, "Faixa Cobre"), 
-	SILVER(9, "Faixa Prateada"), 
-	GOLD(10, "Faixa Dourada");
-	
-	private int level;
-	private String color;
-	
-	Rank(int level, String color) {
-		this.level = level;
-		this.color = color;
-	}
-	
-	public int getLevel() {
-		return level;
-	}
-	
-	public String getColor() {
-		return color;
-	}
+    // TODO - get the values com messages.properties.
+    WHITE(1, "Faixa Branca"),
+    YELLOW(2, "Faixa Amarela"),
+    ORANGE(3, "Faixa Laranjada"),
+    RED(4, "Faixa Vermelha"),
+    PURPLE(5, "Faixa Roxa"),
+    BROWN(6, "Faixa Marrom"),
+    BLACK(7, "Faixa Preta"),
+    COPPER(8, "Faixa Cobre"),
+    SILVER(9, "Faixa Prateada"),
+    GOLD(10, "Faixa Dourada");
+
+    private final int level;
+    private final String color;
+
+    Rank(int level, String color) {
+        this.level = level;
+        this.color = color;
+    }
 }
