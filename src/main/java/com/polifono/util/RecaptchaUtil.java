43c9596fa71dac4b36778568498ef4d3a2a9e3e9
Flavio Moises de Araujo <flavio.dearaujo@ticketmaster.com>
2024-10-10 21:19:06 -0400

    refactoring controller and util

--- src/main/java/com/polifono/util/RecaptchaUtil.java
+++ src/main/java/com/polifono/util/RecaptchaUtil.java
@@ -5,17 +5,13 @@ import java.util.Map;
 
 public class RecaptchaUtil {
 
-	public static final Map<String, String> RECAPTCHA_ERROR_CODE = new HashMap<>();
+    public static final Map<String, String> RECAPTCHA_ERROR_CODE = new HashMap<>();
+
     static {
-    	RECAPTCHA_ERROR_CODE.put("missing-input-secret", 
-    			"The secret parameter is missing");
-    	RECAPTCHA_ERROR_CODE.put("invalid-input-secret", 
-    			"The secret parameter is invalid or malformed");
-    	RECAPTCHA_ERROR_CODE.put("missing-input-response", 
-    			"The response parameter is missing");
-    	RECAPTCHA_ERROR_CODE.put("invalid-input-response", 
-    			"The response parameter is invalid or malformed");
-    	RECAPTCHA_ERROR_CODE.put("bad-request", 
-    			"The request is invalid or malformed");
+        RECAPTCHA_ERROR_CODE.put("missing-input-secret", "The secret parameter is missing");
+        RECAPTCHA_ERROR_CODE.put("invalid-input-secret", "The secret parameter is invalid or malformed");
+        RECAPTCHA_ERROR_CODE.put("missing-input-response", "The response parameter is missing");
+        RECAPTCHA_ERROR_CODE.put("invalid-input-response", "The response parameter is invalid or malformed");
+        RECAPTCHA_ERROR_CODE.put("bad-request", "The request is invalid or malformed");
     }
 }
