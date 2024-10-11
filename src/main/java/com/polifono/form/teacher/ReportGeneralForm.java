877a85ee5835687bb0c3f209aadb0c80bdd951bf
Flavio Moises de Araujo <flavio.dearaujo@ticketmaster.com>
2024-10-10 20:26:53 -0400

    refactoring form

--- src/main/java/com/polifono/form/teacher/ReportGeneralForm.java
+++ src/main/java/com/polifono/form/teacher/ReportGeneralForm.java
@@ -2,47 +2,18 @@ package com.polifono.form.teacher;
 
 import com.polifono.domain.Game;
 
+import lombok.Data;
+
+@Data
 public class ReportGeneralForm {
 
-	private Game game;
-	private com.polifono.domain.Class clazz;
-	private int phaseBegin;
-	private int phaseEnd;
-	
-	public ReportGeneralForm() {
-		this.game = new Game();
-		this.clazz = new com.polifono.domain.Class();
-	}
+    private Game game;
+    private com.polifono.domain.Class clazz;
+    private int phaseBegin;
+    private int phaseEnd;
 
-	public Game getGame() {
-		return game;
-	}
-
-	public void setGame(Game game) {
-		this.game = game;
-	}
-
-	public com.polifono.domain.Class getClazz() {
-		return clazz;
-	}
-
-	public void setClazz(com.polifono.domain.Class clazz) {
-		this.clazz = clazz;
-	}
-
-	public int getPhaseBegin() {
-		return phaseBegin;
-	}
-
-	public void setPhaseBegin(int phaseBegin) {
-		this.phaseBegin = phaseBegin;
-	}
-
-	public int getPhaseEnd() {
-		return phaseEnd;
-	}
-
-	public void setPhaseEnd(int phaseEnd) {
-		this.phaseEnd = phaseEnd;
-	}
+    public ReportGeneralForm() {
+        this.game = new Game();
+        this.clazz = new com.polifono.domain.Class();
+    }
 }
